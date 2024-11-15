939a6d22a6809c0713683ed5f42d0e11b8ca4348
Alithebest-coder <aurumprint@proton.me>
2024-11-15 10:42:01 +1000

    Add files via upload

---
+++ winter.py
@@ -0,0 +1,8 @@
+x =int(input("what is the temptuture today?"))
+
+if x==0:   
+    print("too cold to go outside")
+if x==8 or x==10 or x==12:    
+    print(x ," degree Wear a coat")
+if x==14:
+    print(x,"degree today Light jacet")
